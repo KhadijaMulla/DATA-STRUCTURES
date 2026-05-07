62b55d1280c09d4c511c509b2671138d7c0b9d1c
KhadijaMulla <khadijamulla93493@gmail.com>
2026-05-07 22:20:00 +0530

    Add files via upload

---
+++ DS10THEXPT/Q1.java
@@ -0,0 +1,54 @@
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+class BST {
+    Node root;
+
+    Node insert(Node root, int data) {
+        if (root == null) {
+            root = new Node(data);
+            return root;
+        }
+
+        if (data < root.data) {
+            root.left = insert(root.left, data);
+        } else {
+            root.right = insert(root.right, data);
+        }
+
+        return root;
+    }
+
+    void inorder(Node root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+    }
+}
+
+public class Q1 {
+    public static void main(String[] args) {
+
+        BST tree = new BST();
+
+        tree.root = tree.insert(tree.root, 50);
+        tree.insert(tree.root, 30);
+        tree.insert(tree.root, 70);
+        tree.insert(tree.root, 20);
+        tree.insert(tree.root, 40);
+        tree.insert(tree.root, 60);
+        tree.insert(tree.root, 80);
+
+        System.out.print("BST Inorder Traversal: ");
+        tree.inorder(tree.root);
+    }
+}
